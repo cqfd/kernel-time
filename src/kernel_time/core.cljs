8ac0e01d4bb4123c4c0dfda1b8087084c3d2f3a9
Alan O'Donnell <alan.m.odonnell@gmail.com>
2014-04-11 18:07:18 -0400

    sadface...

--- src/kernel_time/core.cljs
+++ src/kernel_time/core.cljs
@@ -1,45 +1,49 @@
 (ns kernel-time.core
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [om.core :as om :include-macros true]
+  (:require [cljs.nodejs :as n]
+            [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [put! chan <! timeout]]))
 
 (enable-console-print!)
 
-(def request (js/require "request"))
-;;(def torrent-stream (js/require "torrent-stream"))
-(def peerflix (js/require "peerflix"))
+(def t (n/require "torrent-stream"))
+(def http (n/require "http"))
+(def pump (n/require "pump"))
 
 (def app-state (atom {:src nil}))
 
-;; get-request :: URL -> Chan Resp
-(defn get-request [url]
-  (let [ch (chan)]
-    (request url (fn [error response body]
-                   (println "We got something!!")
-                   (put! ch [error response body])))
-    ch))
-
 (def magnet "magnet:?xt=urn:btih:8C5043DF1A8FEC9F7FDDDA70617C6BB96E14E6CC&dn=game+of+thrones+s04e01+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.ex.ua%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337")
 
-(let [engine (peerflix magnet #js {:buffer (str (* 1.5 1024 1024))
-                                   :connections 100})]
-  (. js/console dir engine)
-  (. js/console dir (.-on engine))
-  (.on engine "ready" (fn []
-                        (println "Engine is ready...")
-                        (.listen (.-server engine) 8888)))
-  (.on engine "listening"
-       (fn []
-         (println "We're listening....")
-         (let [server (.-server engine)
-               href (str "http://127.0.0.1:"
-                         (.-port (.address server))
-                         "/")]
-           (update-in app-state [:src]
-                      (fn [_] href))
-           (println @app-state)))))
+(defn torrent->stream [torrent]
+  (let [ch (chan)
+        engine (t torrent)]
+    (.on engine "ready"
+         (fn []
+           (let [f (aget (.-files engine) 1)]
+             (. js/console dir f)
+             (put! ch (.createReadStream f)))))
+    ch))
 
+(defn serve [torrent]
+  (go (let [stream (<! (torrent->stream torrent))
+            server (.createServer http
+                                  (fn [_req res]
+                                    (pump stream res
+                                          (fn [err]
+                                            (.dir js/console err)))))]
+        (.on server "close" (fn [& rest]
+                              (println "server closed!")))
+        (.listen server 8880)
+        (println "Listening...")
+        (swap! app-state update-in [:src] (fn [_] "http://127.0.0.1:8880/"))
+        )))
+
+(serve magnet)
+
+(go (while true
+      (<! (timeout 2000))
+      (println "Tick...")))
 
 (defn video-widget [data owner]
   (reify
